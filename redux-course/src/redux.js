67cae9fee5983b13928ce32cf25b7a1080d1e24c
Wladiosan <52457283+Wladiosan@users.noreply.github.com>
2021-03-30 12:19:47 +0300

    add redux course

---
+++ redux-course/src/redux.js
@@ -0,0 +1,24 @@
+const redux = require('redux')
+
+// Reducer
+
+const reducer1 = () => {
+    return {
+
+    }
+}
+
+const initialState = {
+    counter: 0
+}
+
+const reducer = (state = initialState, action)
+
+
+// Store
+
+const store = redux.createStore(reducer)
+
+
+
+// Action
