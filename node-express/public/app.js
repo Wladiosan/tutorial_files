b8278698e41ee746d1782186ff2bea766303e097
Wladiosan <w.ohrimenko@gmail.com>
2021-07-26 23:04:09 +0300

    add CSRF security for all pages

--- node-express/public/app.js
+++ node-express/public/app.js
@@ -30,14 +30,18 @@ if ($card) {
     $card.addEventListener('click', event => {
         if (event.target.classList.contains('js-remove')) {
             const id = event.target.dataset.id
+            const csrf = event.target.dataset.csrf
             
             fetch('/card/remove/' + id, {
-                method: 'delete'
+                method: 'delete',
+                headers: {
+                    'X-XSRF-TOKEN': csrf
+                }
             }).then(res => res.json())
             .then(card => {
                 if(card.courses.length) {
                     const html = card.courses.map(c => {
-                        return `<tr><td>${c.title}</td><td>${c.count}</td><td><button class="btn btn-small js-remove" data-id="${c.id}">Delete</button></td></tr>`
+                        return `<tr><td>${c.title}</td><td>${c.count}</td><td><button class="btn btn-small js-remove" data-id="${c.id}" data-csrf="${csrf}">Delete</button></td></tr>`
                     }).join('')
                     $card.querySelector('tbody').innerHTML = html
                     $card.querySelector('.price').textContent = toCurrency(card.price)
