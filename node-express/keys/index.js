b8f80fb97f94b87697cb53041167f0152967d9e8
Wladiosan <w.ohrimenko@gmail.com>
2021-07-30 17:29:20 +0300

    setting email service

--- node-express/keys/index.js
+++ node-express/keys/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     MONGODB_URI: 'mongodb+srv://wladios:bnqVMAA2Lb1Q2OSr@cluster0.34yzy.mongodb.net/shop?retryWrites=true&w=majority',
-    SESSION_SECRET: 'some secret value'
+    SESSION_SECRET: 'some secret value',
+    SENDGRID_API_KEY: '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'
 }
