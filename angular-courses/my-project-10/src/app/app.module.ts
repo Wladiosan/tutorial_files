8f9fca916406e1735bb7f0d9ad607dadafdcd562
Wladiosan <w.ohrimenko@gmail.com>
2022-01-09 03:20:49 +0200

    update app.module with a new changes

--- angular-courses/my-project-10/src/app/app.module.ts
+++ angular-courses/my-project-10/src/app/app.module.ts
@@ -1,16 +1,32 @@
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
+import {NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
+import {FormsModule} from "@angular/forms"
 
-import { AppComponent } from './app.component'
+import {AppRoutingModule} from './app-routing.module'
+import {PageNamePipe} from './shared/page-name.pipe'
+import {ColorDirective} from './shared/color.directive'
+
+import {AppComponent} from './app.component'
+import {HomePageComponent} from './home-page/home-page.component'
+import {AboutPageComponent} from './about-page/about-page.component'
+import {AboutExtraPageComponent} from './about-page/about-extra-page/about-extra-page.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    AboutPageComponent,
+    AboutExtraPageComponent,
+    PageNamePipe,
+    ColorDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
