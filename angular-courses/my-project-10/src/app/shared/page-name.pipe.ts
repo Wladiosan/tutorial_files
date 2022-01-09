26d05ef7ed87fd61088518f2d2f671a6c5dcc1f5
Wladiosan <w.ohrimenko@gmail.com>
2022-01-09 03:16:32 +0200

    add a new pipe 'pageName'

---
+++ angular-courses/my-project-10/src/app/shared/page-name.pipe.ts
@@ -0,0 +1,14 @@
+import {Pipe, PipeTransform} from '@angular/core'
+
+
+@Pipe({
+  name: 'pageName'
+})
+
+export class PageNamePipe implements PipeTransform {
+
+  transform(value: string): string {
+    return value + '!!!'
+  }
+
+}
