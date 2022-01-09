eb67ef8272a7d743dc281dc80e979a11d49df4ae
Wladiosan <w.ohrimenko@gmail.com>
2022-01-09 04:31:21 +0200

    create a new module for about-pages

---
+++ angular-courses/my-project-10/src/app/about-page/about-page.module.ts
@@ -0,0 +1,40 @@
+import {NgModule} from '@angular/core'
+import {RouterModule} from '@angular/router'
+import {CommonModule} from '@angular/common'
+
+import {SharedModule} from '../shared/shared.module'
+
+import {AboutPageComponent} from './about-page.component'
+import {AboutExtraPageComponent} from './about-extra-page/about-extra-page.component'
+
+@NgModule({
+  declarations: [
+    AboutPageComponent,
+    AboutExtraPageComponent
+  ],
+  imports: [
+    SharedModule,
+    CommonModule,
+    RouterModule.forChild([
+      // http://localhost:4200/about
+      {
+        path: 'about',
+        component: AboutPageComponent,
+        children: [
+          // http://localhost:4200/about/extra
+          {
+            path: 'extra',
+            component: AboutExtraPageComponent
+          }
+        ]
+      }
+    ])
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+
+export class AboutPageModule {
+
+}
