a1e0b7af45910547194c8476ddbb8554b7b1b2e3
Wladiosan <w.ohrimenko@gmail.com>
2022-01-09 05:05:19 +0200

    add a strategy preloadingStrategy for root Router

--- angular-courses/my-project-10/src/app/app-routing.module.ts
+++ angular-courses/my-project-10/src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import {NgModule} from '@angular/core'
-import {RouterModule} from '@angular/router'
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router'
 
 import {HomePageComponent} from './home-page/home-page.component'
 
+const routes: Routes = [
+  // http://localhost:4200
+  {path: '', component: HomePageComponent, pathMatch: 'full'},
+  {path: 'about', loadChildren: () => import('./about-page/about-page.module').then(m => m.AboutPageModule)}
+]
+
 @NgModule({
-  imports: [RouterModule.forRoot([
-      // http://localhost:4200
-      {path: '', component: HomePageComponent, pathMatch: 'full'},
-      {path: 'about', loadChildren: () => import('./about-page/about-page.module').then(m => m.AboutPageModule)}
-    ]
-  )],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 
