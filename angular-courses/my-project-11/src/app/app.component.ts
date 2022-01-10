ee7c6490aadc01076de5dd91c499b2fa1b3597bf
Wladiosan <w.ohrimenko@gmail.com>
2022-01-10 16:49:52 +0200

    SEO, use tags

--- angular-courses/my-project-11/src/app/app.component.ts
+++ angular-courses/my-project-11/src/app/app.component.ts
@@ -1,7 +1,8 @@
 import {Component, ComponentFactoryResolver, ViewChild} from '@angular/core'
+import {Meta, Title} from '@angular/platform-browser'
 
 import {ModalComponent} from './modal/modal.component'
-import {RefDirective} from "./ref.directive";
+import {RefDirective} from './ref.directive'
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,17 @@ export class AppComponent {
 
   @ViewChild(RefDirective, {static: false}) refDir!: RefDirective
 
-  constructor(private resolver: ComponentFactoryResolver) {}
+  constructor(
+    private resolver: ComponentFactoryResolver,
+    private title: Title,
+    private meta: Meta
+  ) {
+    this.title.setTitle('AppComponetnPage')
+    this.meta.addTags([
+      {name: 'keywords', content: 'angular, google, app component'},
+      {name: 'description', content: 'this is app component'}
+    ])
+  }
 
   showModal() {
     const modalFactory = this.resolver.resolveComponentFactory(ModalComponent)
